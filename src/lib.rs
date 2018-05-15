0819f8cd2aa9ca8aba51a22734e0012580ff5d23
Ozaren <krishna.sd.2012@gmail.com>
2018-05-15 14:48:14 -0400

    reorganized files

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,6 @@
 extern crate rand;
 
-pub mod vector_impl;
-pub mod vector_ops;
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct Vector<T>
-    where T: Clone + Copy {
-    value: Vec<T>
-}
-
-pub type VectorD = Vector<f64>;
-pub type VectorF = Vector<f32>;
-pub type VectorI = Vector<i32>;
-pub type VectorU = Vector<u32>;
-pub type VectorS = Vector<usize>;
+pub mod vector;
 
 #[cfg(test)]
 mod tests;
